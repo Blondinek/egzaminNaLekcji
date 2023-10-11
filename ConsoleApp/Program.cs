a689fbd2a8ec6c914a76445776730839b0b22f95
Komputer HP <Komputer HP@DESKTOP-K2OKBOM>
2023-10-11 12:19:11 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 100;
+        bool[] primes = SieveOfEratosthenes(n);
+
+        Console.WriteLine($"Liczby pierwsze w przedziale od 2 do {n}:");
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (primes[i])
+            {
+                Console.Write($"{i}, ");
+            }
+        }
+
+        Console.WriteLine("\nWszystkie liczby pierwsze zostały podane, naciśnij przycisk by zakończyć program.");
+        Console.ReadKey();
+    }
+
+    static bool[] SieveOfEratosthenes(int n)
+    {
+        bool[] primes = new bool[n + 1];
+        for (int i = 2; i <= n; i++)
+        {
+            primes[i] = true;
+        }
+
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (primes[i])
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        return primes;
+    }
+}
